b327b3015e2b71f0c43afa535af06887b5f20f92
Fábio David Freitas <fabio.david.contato@gmail.com>
2020-10-28 16:07:32 -0300

    Think I finished the models, review before migrating
    DIDN'T MIGRATE YET

--- network/models.py
+++ network/models.py
@@ -3,4 +3,19 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    followers = models.ManyToManyField('self', blank=True, related_name='followers')
+    following = models.ManyToManyField('self', blank=True, related_name='following')
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author')
+    body = models.TextField(max_length=500)
+    time = models.DateTimeField() #Added when saving with the API
+    likes = models.IntegerField(default=0)
+
+class Comment(models.Model):
+    post_commented = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author')
+    body = models.TextField(max_length=500)
+    time = models.DateTimeField() #Added when saving with the API
+    likes = models.IntegerField(default=0)
+
