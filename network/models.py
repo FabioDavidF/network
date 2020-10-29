497c75a0dee4025473f67aa41107add2cee318e7
Fábio David Freitas <fabio.david.contato@gmail.com>
2020-10-29 08:10:01 -0300

    changed the models related names

--- network/models.py
+++ network/models.py
@@ -7,15 +7,15 @@ class User(AbstractUser):
     following = models.ManyToManyField('self', blank=True, related_name='following')
 
 class Post(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='p_author')
     body = models.TextField(max_length=500)
-    time = models.DateTimeField() #Added when saving with the API
+    time = models.DateTimeField() #Added when creating object with the API
     likes = models.IntegerField(default=0)
 
 class Comment(models.Model):
-    post_commented = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post')
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author')
+    post_commented = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='c_post')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='c_author')
     body = models.TextField(max_length=500)
-    time = models.DateTimeField() #Added when saving with the API
+    time = models.DateTimeField() #Added when creating object with the API
     likes = models.IntegerField(default=0)
 
