aa855bc66285409b27c052c8678c911c95326562
Fábio David Freitas <fabio.david.contato@gmail.com>
2020-10-30 18:16:48 -0300

    Added .username to user in serialize

--- network/models.py
+++ network/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
     def serialize(self):
         return {
             'id': self.id,
-            'author': self.author,
+            'author': self.author.username,
             'body': self.body,
             'time': self.time,
             'likes': self.likes
